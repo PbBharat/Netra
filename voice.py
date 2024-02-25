7c377e7dbb97a60b690e088b199d42346ce3e859
PbBharat <154305502+PbBharat@users.noreply.github.com>
2024-02-25 08:51:55 +0530

    Create voice.py

---
+++ voice.py
@@ -0,0 +1,37 @@
+import os
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = "/home/aneesh.paul/Downloads/team-medy-buddy-aa4016a467d2.json"
+
+from google.cloud import texttospeech
+
+def text_to_voice(text_str):
+    # Instantiates a client
+    client = texttospeech.TextToSpeechClient()
+
+    # Set the text input to be synthesized
+    synthesis_input = texttospeech.SynthesisInput(text=text_str)
+
+    # Build the voice request, select the language code ("en-US") and the ssml
+    # voice gender ("neutral")
+    voice = texttospeech.VoiceSelectionParams(
+        language_code="hi-IN", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+    )
+
+    # Select the type of audio file you want returned
+    audio_config = texttospeech.AudioConfig(
+        audio_encoding=texttospeech.AudioEncoding.MP3
+    )
+
+    # Perform the text-to-speech request on the text input with the selected
+    # voice parameters and audio file type
+    response = client.synthesize_speech(input=synthesis_input, voice=voice, audio_config=audio_config)
+
+    # The response's audio_content is binary.
+    with open("backend/output.mp3", "wb") as out:
+        # Write the response to the output file.
+        out.write(response.audio_content)
+        print('Audio content written to file "output.mp3"')
+
+    return response.audio_content
+
+
+# text_to_voice("Hello")
